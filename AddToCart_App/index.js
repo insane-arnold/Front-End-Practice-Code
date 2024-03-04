9d83fbb54bb086b7f0745f013ec7661b32b04835
NIHAL LIMBU <48156789+insane-arnold@users.noreply.github.com>
2024-03-04 14:27:51 +0530

    Update index.js

--- AddToCart_App/index.js
+++ AddToCart_App/index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-60ca9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "add Firebase DB API url here"
 }
 
 const app = initializeApp(appSettings)
